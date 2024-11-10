4b040ddce5fd7faa20699e24635b968b1329133f
mPyle05 <mason.d.pyle@gmail.com>
2024-11-10 06:13:42 -0500

    version with Login and repetition

--- InstaCommentMod.py
+++ InstaCommentMod.py
@@ -1,58 +1,65 @@
+import time
+
 from instagrapi import Client
 import re
+import os
+from transformers import pipeline
 
-with open("bad_words.txt", "r") as file:
-    # Strip quotes and split by commas, then strip whitespace around each word
-    bad_words = [word.strip().strip('"') for word in file.read().split(",")]
-
-ipBan = int(input("1 for old, 2 for new: "))
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+pipe = pipeline("text-classification", model="facebook/roberta-hate-speech-dynabench-r4-target")
 cl = Client()
 
-if ipBan == 1:
-    cl.load_settings('dump.json')
-    cl.login("elijahwhitesell25@gmail.com", "BallBall98!")
-    userId = cl.user_id_from_username("ewhi.tesell")
-else:
-    cl.load_settings('dump2.json')
-    cl.login("elijahwhitesel26@gmail.com", "BallBall98!")
-    userId = cl.user_id_from_username("ewhi.t.esell")
-    #cl.dump_settings('dump2.json')
+username = input("PLease enter your username: ")
+password = input("Please enter your password: ")
+tick = int(input("Please enter the time between checks (in seconds): "))
+
+cl.login(username, password)
+userId = cl.user_id_from_username(username)
 
 print("userId:" + userId)
 
-userMedia = cl.user_medias(userId, 5)
+while True:
 
-mediaId = ""
-commentCount = 0
-badCommentIds = []
+    userMedia = cl.user_medias(userId, 5)
+    mediaId = ""
+    commentCount = 0
+    badCommentIds = []
 
-for i in userMedia:
-    print(i)
-    mediaId = re.search(r"id='(.*?)'", str(i))
-    mediaId = mediaId.group(1)
-    print(" From USER profile" + mediaId)
-    commentCount = re.search(r"comment_count='(.*?)'", str(i))
+    for i in userMedia:
+    #print(i)
+        mediaId = re.search(r"id='(.*?)'", str(i))
+        mediaId = mediaId.group(1)
+        print("Media id: " + mediaId)
+        commentCount = re.search(r"comment_count='(.*?)'", str(i))
 
-    comments = cl.media_comments(mediaId, commentCount)
+        comments = cl.media_comments(mediaId, commentCount)
 
-    for comment in comments:
-        print(comment)
+        for comment in comments:
+            #print(comment)
 
-        match = re.search(r"text='(.*?)'", str(comment))
-        user = re.search(r"username='(.*?)'", str(comment))
-        commentPK = re.search(r"pk='(.*?)'", str(comment))
-        extracted_text = match.group(1)
-        user = user.group(1)
-        commentPK = int(commentPK.group(1))
+            match = re.search(r"text='(.*?)'", str(comment))
+            user = re.search(r"username='(.*?)'", str(comment))
+            commentPK = re.search(r"pk='(.*?)'", str(comment))
+            extracted_text = match.group(1)
+            user = user.group(1)
+            commentPK = int(commentPK.group(1))
 
-        print(match)
-        for word in bad_words:
-            if word.lower() in extracted_text.lower():
-                print(user + f" is bad. Found disliked word: '{word}'")
-                print("PK: " + str(commentPK))
+            #print(match)
+
+            result = pipe(extracted_text)
+
+            # Directly access the label and score from the result
+            label = result[0]['label']
+            #print(label)
+
+            if label == "hate":
                 badCommentIds.append(commentPK)
+                print("PK added to list: " + str(commentPK))
 
-    print(badCommentIds)
-    if cl.comment_bulk_delete(mediaId, badCommentIds):
-        print("Deleted comments")
-        badCommentIds.clear()
+        print(badCommentIds)
+        if badCommentIds:
+            if cl.comment_bulk_delete(mediaId, badCommentIds):
+                print("Deleted comments")
+                badCommentIds.clear()
+
+    time.sleep(tick)
