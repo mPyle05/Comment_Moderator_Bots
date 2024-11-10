89fd98f47a029821f30c31a16d82d4e7ca9c6119
Rhys0506 <vismays0506@gmail.com>
2024-11-10 08:35:41 -0500

    Reddit-Bot-Final.py

--- Reddit-Bot-Final.py
+++ Reddit-Bot-Final.py
@@ -1,56 +1,39 @@
 import praw
 from praw.exceptions import RedditAPIException
 import time
-import logging
-logging.basicConfig(level = logging.DEBUG)
+import os
+from transformers import pipeline
 
-message = 'boo'
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+pipe = pipeline("text-classification", model="facebook/roberta-hate-speech-dynabench-r4-target")
+
 reddit = praw.Reddit(
-    client_id='wG7NSQTDHsXtHjhNA8_Oew',
-    client_secret='YhSRTfyvrDj4vAKNgy43qYMQ5qjfdg',
+    client_id='Enter client_id',
+    client_secret='Enter secret key',
     user_agent="<console:Objective-Job1591:1.0>",
-    username = 'Objective-Job1591',
-    password = 'MakeUC002'
+    username = 'Enter account username',
+    password = 'Enter account password'
 )
 
-with open("bad_words.txt", "r") as file:
-    # Strip quotes and split by commas, then strip whitespace around each word
-    bad_words = [word.strip().strip('"') for word in file.read().split(",")]
 
-
-
-subreddit = reddit.subreddit("MakeUCTest2")
+subreddit = reddit.subreddit("Enter the name of the subreddit you want to moderate")
 for submission in subreddit.hot(limit=10):
     if submission.stickied == False:
         for comment in submission.comments:
             if hasattr(comment,"body"):
 
                 comment_lower = comment.body.lower()
-                for word in bad_words:
-                    word_string = str(word.lower())
-                    if word in comment_lower:   
-                        print(comment_lower)
-                        print('--------------')
-                        try:
-                            comment_id = comment.id 
-                            comment = reddit.comment(id=comment_id)
-                            comment.mod.remove()
-                        except RedditAPIException as e:
+                result = pipe(comment_lower)
+                label = result[0]['label']
+                if label == 'hate':
+                    print(comment_lower)
+                    print('--------------')
+                    try:
+                        comment_id = comment.id 
+                        comment = reddit.comment(id=comment_id)
+                        comment.mod.remove()
+                    except RedditAPIException as e:
 
-                            print(f"an error occured: {e}")
-                        time.sleep(10)
-                        
-
+                        print(f"an error occured: {e}")
+                    time.sleep(10)
            
-
-           
-
-'''
-
-'''
-
-
-
-
-
-
